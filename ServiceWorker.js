6704dd7ac170e1c384f6f26a83cefd1d87ccc209
gftyukj <amir@golestane.com>
2026-06-04 08:14:49 -0700

    update w

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "MasterGames-Duality-1.0";
 const contentToCache = [
-    "Build/update 999999999w.loader.js",
-    "Build/update 999999999w.framework.js.unityweb",
-    "Build/update 999999999w.data.unityweb",
-    "Build/update 999999999w.wasm.unityweb",
+    "Build/update 99999999999990.loader.js",
+    "Build/update 99999999999990.framework.js.unityweb",
+    "Build/update 99999999999990.data.unityweb",
+    "Build/update 99999999999990.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
