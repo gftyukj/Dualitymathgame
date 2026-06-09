9fb68648bd7e8fa195c9f7a67a026dbd69cdd6b8
gftyukj <amir@golestane.com>
2026-06-08 19:51:58 -0700

    wwwwwwwwww kool

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "MasterGames-Duality-1.0";
-const contentToCache = [
-    "Build/update final 01.loader.js",
-    "Build/update final 01.framework.js.unityweb",
-    "Build/update final 01.data.unityweb",
-    "Build/update final 01.wasm.unityweb",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
